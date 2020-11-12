3cceb6ee72fd78e1b6b8bd5ad8e710cae24d6352
toniihrd <toniihrd@gmail.com>
2020-11-12 22:28:29 +0000

    extracted data needed

--- TicketViewer/src/app/dashboard/dashboard.component.ts
+++ TicketViewer/src/app/dashboard/dashboard.component.ts
@@ -6,16 +6,27 @@ import { UserserviceService } from '../userservice.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+l
+ TicketTilte=['id','author_id','body','created_at','edited_at','url','section_id','updated_at']
+  TicketDetails: any;
   constructor(private usersrv: UserserviceService) { }
-
   ngOnInit(): void {
     this.usersrv.update().subscribe(x => {
-      
-      console.log(x,'hhh')
+     // console.log(x["articles"][0])
+
+
+
+      for(let i = 0; i < x["count"]; i++) 
+      {this.TicketDetails=x["articles"][i]
+        for(let j in this.TicketTilte)
+        {
+          console.log(this.TicketDetails[this.TicketTilte[j]])
+        }
+        
+      }
 
     });
-    console.log('hhh')
+
 
   }
 

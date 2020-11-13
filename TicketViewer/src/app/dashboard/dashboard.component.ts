524bc4138ff45613352cb4c3c6dcd4598b3c7c09
toniihrd <toniihrd@gmail.com>
2020-11-13 23:38:16 +0000

    listed tickets as html page

--- TicketViewer/src/app/dashboard/dashboard.component.ts
+++ TicketViewer/src/app/dashboard/dashboard.component.ts
@@ -6,25 +6,20 @@ import { UserserviceService } from '../userservice.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-l
- TicketTilte=['id','author_id','body','created_at','edited_at','url','section_id','updated_at']
+  TicketTilte = ['id', 'author_id', 'body', 'created_at', 'edited_at', 'url', 'section_id', 'updated_at']
   TicketDetails: any;
   constructor(private usersrv: UserserviceService) { }
   ngOnInit(): void {
     this.usersrv.update().subscribe(x => {
-     // console.log(x["articles"][0])
-
-
-
-      for(let i = 0; i < x["count"]; i++) 
-      {this.TicketDetails=x["articles"][i]
-        for(let j in this.TicketTilte)
-        {
-          console.log(this.TicketDetails[this.TicketTilte[j]])
+              for (let i = 0; i < x["count"]; i++) 
+      {
+        document.write("Ticket Number:"+(i+1)+"<br>")
+        this.TicketDetails = x["articles"][i]
+        for (let j in this.TicketTilte) {
+          document.write( this.TicketTilte[j]+" :"+this.TicketDetails[this.TicketTilte[j]]+"<br>")
         }
-        
+        document.write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+"<br><br>")
       }
-
     });
 
 
